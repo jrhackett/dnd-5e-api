8d228ed15ca3ea6bbf0c634696323d2b35f18875
Jacob Hackett <jacobrhackett@gmail.com>
2017-05-27 12:09:35 -0400

    cleaning up repetitive code and moving over to ES6 syntax

--- app/routes/spells.js
+++ app/routes/spells.js
@@ -1,58 +1,49 @@
 var Spell = require('../models/spell')
 
-module.exports = function(app) {
+module.exports = (app) => {
 
-  app.get('/spells', function(req, res) {
-    Spell.find({}).exec(function (err, items) {
-      if (err) {
-        return console.log(err);
-      }
-      res.send(items)
-    })
+  app.get('/spells', (req, res) => {
+    filterSpells({}, res)
   })
 
-  app.get('/spells/level/:level?', function(req, res) {
+  app.get('/spells/level/:level?', (req, res) => {
     var level = req.params.level
     if (!level) {
-        next()
-        return
+      next()
+      return
     }
-    Spell.find({level:level}).exec(function (err, items) {
-      if (err) {
-        return console.log(err);
-      }
-      res.send(items)
-    })
+    filterSpells({level:level}, res)
   })
 
-  app.get('/spells/school/:school?', function(req, res) {
+  app.get('/spells/school/:school?', (req, res) => {
     var school = req.params.school
     if (!school) {
-        next()
-        return
+      next()
+      return
     }
-    school = school.charAt(0).toUpperCase() + school.slice(1).toLowerCase()
-    Spell.find({school:school}).exec(function (err, items) {
-      if (err) {
-        return console.log(err);
-      }
-      res.send(items)
-    })
+    filterSpells({school:capitalize(school)}, res)
   })
 
-  app.get('/spells/class/:className?', function(req, res) {
+  app.get('/spells/class/:className?', (req, res) => {
     var className = req.params.className
     if (!className) {
-        next()
-        return
+      next()
+      return
     }
-    className = className.charAt(0).toUpperCase() + className.slice(1).toLowerCase()
-    Spell.find({classes:className}).exec(function (err, items) {
-      if (err) {
-        return console.log(err);
-      }
-      res.send(items)
-    })
+    filterSpells({classes:capitalize(className)}, res)
   })
-  
+
+}
+
+const filterSpells = (filter, res) => {
+  Spell.find(filter).exec( (err, items) => {
+    if (err) {
+      return console.log(err);
+    }
+    res.send(items)
+  })
+}
+
+const capitalize = (word) => {
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
 }
