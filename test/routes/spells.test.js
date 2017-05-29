84dc2e3a048c96c4de3ee865eb13b3e52c16e5ea
Jacob Hackett <jacobrhackett@gmail.com>
2017-05-28 20:35:30 -0400

    adding tests for the response content

--- test/routes/spells.test.js
+++ test/routes/spells.test.js
@@ -18,45 +18,85 @@ describe('server responds appropriately', () => {
       .expect(200, done)
   })
 
-  // TODO test the response text
   it('responds to /spells', (done) => {
     request(server)
       .get('/spells')
+      .expect('Content-Type', /json/)
       .expect(200, done)
   })
 
-  // TODO test the response text
   it('responds to /spells/level/:level', (done) => {
     for(let i = 0; i < 10; i++) {
       request(server)
         .get('/spells/level/' + i)
+        .expect('Content-Type', /json/)
         .expect(200, done)
+        .expect( (res) => {
+          res.body.map( (obj) => {
+            if(obj.level != i)
+              return false
+          })
+          return true
+        })
     }
   })
 
-  // TODO test the response text
   it('responds to /spells/school/:school', (done) => {
     let schools = ['Abjuration', 'Conjuration', 'Divination', 'Enchantment', 'Evocation', 'Illusion', 'Necromancy', 'Transmutation']
     for(var school in schools) {
       request(server)
         .get('/spells/school/' + schools[school])
+        .expect('Content-Type', /json/)
         .expect(200, done)
+        .expect( (res) => {
+          res.body.map( (obj) => {
+            if(obj.school !== i)
+              return false
+          })
+          return true
+        })
     }
   })
 
-  // TODO test the response text
   it('responds to /spells/class/bard', (done) => {
     let classes = ['Bard', 'Cleric', 'Druid', 'Paladin', 'Ranger', 'Sorcerer', 'Warlock', 'Wizard']
     for(var className in classes) {
       request(server)
         .get('/spells/class/' + classes[className])
+        .expect('Content-Type', /json/)
         .expect(200, done)
+        .expect( (res) => {
+          res.body.map( (obj) => {
+            var myClasses = new Set(obj.classes)
+            if(!myClasses.has(className))
+              return false
+          })
+          return true
+        })
     }
   })
 
-  it('404 everything else', (done) => {
+  it('404 random URL', (done) => {
     request(server)
       .get('/foo/bar')
+      .expect(404, done)
+  });
+
+  it('404 incomplete level', (done) => {
+    request(server)
+      .get('/level')
+      .expect(404, done)
+  });
+
+  it('404 incomplete school', (done) => {
+    request(server)
+      .get('/school')
+      .expect(404, done)
+  });
+
+  it('404 incomplete class', (done) => {
+    request(server)
+      .get('/class')
       .expect(404, done)
   });
 })
