23cbf498e1a3c0f139829917d6fa9e4f3d55b996
Jacob Hackett <jacobrhackett@gmail.com>
2017-09-04 14:54:17 -0400

    refactor filter links

--- client/src/components/spells/SpellFilters.jsx
+++ client/src/components/spells/SpellFilters.jsx
@@ -2,43 +2,34 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import SpellFilterLink from './SpellFilterLink'
 
-const SpellFilters = ({ shouldShow, onFilterLevelClick, onFilterSchoolClick, onFilterClassClick, onFilterClear }) => {
+const levels = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
+const schools = ['Abjuration', 'Conjuration', 'Divination', 'Enchantment',
+  'Evocation', 'Illusion', 'Necromancy', 'Transmutation']
+const classes = ['Bard', 'Cleric', 'Druid', 'Paladin', 'Ranger',
+  'Sorcerer', 'Warlock', 'Wizard']            
+
+const SpellFilters = ({ shouldShow, onFilterLevelClick, onFilterSchoolClick, onFilterClassClick, 
+  onFilterClear }) => {
   if(shouldShow) {
     return (
       <div>
         <div className="filter-links">
-          <span>Filter Level: </span>
-          <SpellFilterLink text={'0'} onClick={onFilterLevelClick} />
-          <SpellFilterLink text={'1'} onClick={onFilterLevelClick} />
-          <SpellFilterLink text={'2'} onClick={onFilterLevelClick} />
-          <SpellFilterLink text={'3'} onClick={onFilterLevelClick} />
-          <SpellFilterLink text={'4'} onClick={onFilterLevelClick} />
-          <SpellFilterLink text={'5'} onClick={onFilterLevelClick} />
-          <SpellFilterLink text={'6'} onClick={onFilterLevelClick} />
-          <SpellFilterLink text={'7'} onClick={onFilterLevelClick} />
-          <SpellFilterLink text={'8'} onClick={onFilterLevelClick} />
+          <span className="filter-label">Level: </span>
+          {levels.map(level => {
+            return <SpellFilterLink key={level} text={level} onClick={onFilterLevelClick} />
+          })}
         </div>
         <div className="filter-links">
-          <span>Filter School: </span>
-          <SpellFilterLink text={'Abjuration'} onClick={onFilterSchoolClick} />
-          <SpellFilterLink text={'Conjuration'} onClick={onFilterSchoolClick} />
-          <SpellFilterLink text={'Divination'} onClick={onFilterSchoolClick} />
-          <SpellFilterLink text={'Enchantment'} onClick={onFilterSchoolClick} />
-          <SpellFilterLink text={'Evocation'} onClick={onFilterSchoolClick} />
-          <SpellFilterLink text={'Illusion'} onClick={onFilterSchoolClick} />
-          <SpellFilterLink text={'Necromancy'} onClick={onFilterSchoolClick} />
-          <SpellFilterLink text={'Transmutation'} onClick={onFilterSchoolClick} />
+          <span className="filter-label">School: </span>
+          {schools.map(school => {
+            return <SpellFilterLink key={school} text={school} onClick={onFilterSchoolClick} />
+          })}
         </div>
         <div className="filter-links">
-          <span>Filter Class: </span>
-          <SpellFilterLink text={'Bard'} onClick={onFilterClassClick} />
-          <SpellFilterLink text={'Cleric'} onClick={onFilterClassClick} />
-          <SpellFilterLink text={'Druid'} onClick={onFilterClassClick} />
-          <SpellFilterLink text={'Paladin'} onClick={onFilterClassClick} />
-          <SpellFilterLink text={'Ranger'} onClick={onFilterClassClick} />
-          <SpellFilterLink text={'Sorcerer'} onClick={onFilterClassClick} />
-          <SpellFilterLink text={'Warlock'} onClick={onFilterClassClick} />
-          <SpellFilterLink text={'Wizard'} onClick={onFilterClassClick} />
+          <span className="filter-label">Class: </span>
+          {classes.map(className => {
+            return <SpellFilterLink key={className} text={className} onClick={onFilterClassClick} />
+          })}
         </div>
         <div>
           <a onClick={e => {
