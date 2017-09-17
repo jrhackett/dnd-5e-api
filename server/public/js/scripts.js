4d38e9e3c852e5fc84bd816368b11ac69a804b31
Jacob Hackett <jacobrhackett@gmail.com>
2017-09-17 11:23:38 -0400

    removing public folder left over from the swagger-like api docs

--- server/public/js/scripts.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-  $("#example-form").submit(function(event) {
-    event.preventDefault();
-    $.get($("#example-form input").val(), function(data) {
-      $("#result code").html(JSON.stringify(data, null, 2));
-    });
-  });
-})(jQuery);
