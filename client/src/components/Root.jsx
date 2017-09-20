a524d782f3068242bbbb30934e9255def2dc88bb
Jacob Hackett <jacobrhackett@gmail.com>
2017-09-20 19:06:58 -0400

    adding start to navbar

--- client/src/components/Root.jsx
+++ client/src/components/Root.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 import RootSpells from './spells/RootSpells'
 import RootSpellbook from './spellbook/RootSpellbook'
 import RootProfile from './profile/RootProfile'
@@ -12,11 +12,20 @@ const Root = ({ store }) => (
   <Provider store={store}>
     <div>
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={RootSpells} />
-          <Route exact path="/spellbook" component={RootSpellbook} />
-          <Route exact path="/profile" component={RootProfile} />
-        </Switch>
+        <div>
+          <nav id="nav-main">
+            <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/'>Spells</Link></li>
+              <li><Link to='/'>Spellbook</Link></li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={RootSpells} />
+            <Route exact path="/spellbook" component={RootSpellbook} />
+            <Route exact path="/profile" component={RootProfile} />
+          </Switch>
+        </div>
       </BrowserRouter>
     </div>
   </Provider>
