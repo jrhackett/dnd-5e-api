aebb45510c1d444b6343b7a56d9eddc6c1373780
Jacob Hackett <jacobrhackett@gmail.com>
2017-09-23 10:54:18 -0400

    abstracting out a navigation component

---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navigation = () => (
+  <nav className="navbar">
+    <ul className="nav">
+      <li className="item"><Link to='/'>Spells</Link></li>
+      <li className="item"><Link to='/spellbook'>Spellbook</Link></li>
+    </ul>
+  </nav>
+)
+
+export default Navigation
--- client/src/components/Root.jsx
+++ client/src/components/Root.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Navigation from './Navigation'
 import RootSpells from './spells/RootSpells'
 import RootSpellbook from './spellbook/RootSpellbook'
 import RootProfile from './profile/RootProfile'
@@ -13,13 +14,7 @@ const Root = ({ store }) => (
     <div>
       <BrowserRouter>
         <div>
-          <nav className="navbar">
-            <ul className="nav">
-              <li className="item"><Link to='/'>Home</Link></li>
-              <li className="item"><Link to='/'>Spells</Link></li>
-              <li className="item"><Link to='/'>Spellbook</Link></li>
-            </ul>
-          </nav>
+          <Navigation />
           <Switch>
             <Route exact path="/" component={RootSpells} />
             <Route exact path="/spellbook" component={RootSpellbook} />
