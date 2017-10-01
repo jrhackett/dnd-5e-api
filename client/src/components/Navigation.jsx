0071b0dc3959e3d8f977059a6cdf1f6c0333b05c
Jacob Hackett <jacobrhackett@gmail.com>
2017-10-01 11:02:08 -0400

    navbar styling

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -4,8 +4,9 @@ import { Link } from 'react-router-dom'
 const Navigation = () => (
   <nav className="navbar">
     <ul className="nav">
-      <li className="item"><Link to='/'>Spells</Link></li>
-      <li className="item"><Link to='/spellbook'>Spellbook</Link></li>
+      <li className="item"><Link to="/">Spells</Link></li>
+      <li className="item"><Link to="/spellbook">Spellbook</Link></li>
+      <li className="item"><Link to="/profile">Profile</Link></li>
     </ul>
   </nav>
 )
