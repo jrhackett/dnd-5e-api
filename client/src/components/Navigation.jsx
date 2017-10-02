678eb4f9c90534c9483a3a07757ec835eac15b8a
Jacob Hackett <jacobrhackett@gmail.com>
2017-10-01 21:12:19 -0400

    adding navbar logo

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom'
 
 const Navigation = () => (
   <nav className="navbar">
+    <ul className="nav logo">
+      <li className="item"><Link to="/">Spellslot</Link></li>
+    </ul>
     <ul className="nav">
       <li className="item"><Link to="/">Spells</Link></li>
       <li className="item"><Link to="/spellbook">Spellbook</Link></li>
