563ad9b4005e03303a1c0fa16cf23948379ff763
Jacob Hackett <jacobrhackett@gmail.com>
2017-10-01 20:57:41 -0400

    formatting

--- client/src/containers/spells/filters/SpellFilterClassContainer.js
+++ client/src/containers/spells/filters/SpellFilterClassContainer.js
@@ -3,22 +3,21 @@ import SpellFilterLink from '../../../components/spells/SpellFilterLink'
 import { filterSpellsByClass, clearSpellsClassFilter } from '../../../actions/spells'
 
 const getActiveState = (state, text) => {
-  return text === 'All' ? state.spells.filterClass.length === 0 :
+  return text === 'All' ? 
+    state.spells.filterClass.length === 0 :
     state.spells.filterClass.includes(text)
 }
 
 const getOnClickFunction = (dispatch, text) => {
-  return text === 'All' ? () => dispatch(clearSpellsClassFilter()) :
+  return text === 'All' ? 
+    () => dispatch(clearSpellsClassFilter()) :
     () => dispatch(filterSpellsByClass(text))
 }
 
-const mapStateToProps = (state, ownProps) => {
-  const text = ownProps.text
-  return {
-    text: text,
-    active: getActiveState(state, text)
-  }
-}
+const mapStateToProps = (state, ownProps) => ({
+  text: ownProps.text,
+  active: getActiveState(state, ownProps.text)
+})
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
   onClick: getOnClickFunction(dispatch, ownProps.text)
