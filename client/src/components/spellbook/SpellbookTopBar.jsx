2c65ff9bed5728052aae497e90275f845f52ab23
Jacob Hackett <jacobrhackett@gmail.com>
2017-10-05 19:08:17 -0400

    adding the ability to search and filter spellbook listings

--- client/src/components/spellbook/SpellbookTopBar.jsx
+++ client/src/components/spellbook/SpellbookTopBar.jsx
@@ -1,16 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Icon } from 'react-fa'
+import SpellFiltersContainer from '../../containers/spells/filters/SpellFiltersContainer'
+import SpellsSearchBar from '../spells/SpellsSearchBar'
 
-const SpellbookTopBar = () => (
-    <div>
-      <div className="container spells-header">
-        <div className="spells-header-left">
-          <h1>Spellbook</h1>
+class SpellbookTopBar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      shouldShowFilters: false
+    }
+  }
+
+  toggleFilters() {
+    this.setState({
+      shouldShowFilters: !this.state.shouldShowFilters
+    })
+  }
+
+  render() {
+    const { onSearchChange } = this.props
+
+    return (
+      <div>
+        <div className="container spells-header">
+          <div className="spells-header-left">
+            <h1>Spellbook</h1>
+          </div>
+          <div className="spells-header-right">
+            <SpellsSearchBar onTermChange={onSearchChange} />
+              <a className="btn btn-default btn-filter" onClick={e => {
+                  e.preventDefault()
+                  this.toggleFilters()
+                }}
+              >
+                <span>Filter</span> <Icon name="filter"></Icon>
+              </a>
+          </div>
         </div>
-        <div className="spells-header-right">
-          <p>Under construction</p>
-        </div>
+        <SpellFiltersContainer shouldShow={ this.state.shouldShowFilters } />
       </div>
-    </div>
-)
+    )
+  }
+}
 
 export default SpellbookTopBar
--- client/src/containers/spellbook/SpellbookListContainer.js
+++ client/src/containers/spellbook/SpellbookListContainer.js
@@ -1,18 +1,27 @@
 import { connect } from 'react-redux'
 import SpellsList from '../../components/spells/SpellsList'
+import { getVisibleSpells } from '../../selectors/spells'
+import { updateSearchTerm } from '../../actions/spells'
 
-const getSpellbookSpells = (state) => {
-  return state.spells.spells.filter((s) => {
+const getSpellbookSpells = (spells, state) => {
+  return spells.filter((s) => {
     return state.spellbook.selected.includes(s.id)
   })
 }
 
 const mapStateToProps = state => ({
-  spells: getSpellbookSpells(state)
+  spells: getSpellbookSpells(getVisibleSpells(state), state)
+})
+
+const mapDispatchToProps = dispatch => ({
+  onSearchChange: (term) => {
+    dispatch(updateSearchTerm(term))
+  }
 })
 
 const SpellbookListContainer = connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(SpellsList)
 
 export default SpellbookListContainer
