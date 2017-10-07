ec52581ccf7edb2e8be9400e7b2cdff17a5aede7
Jacob Hackett <jacobrhackett@gmail.com>
2017-10-07 11:35:13 -0400

    reorganizing root components for each page

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import ProfileTopBar from './ProfileTopBar'
+import ProfileTopBar from './profile/ProfileTopBar'
 
 class RootProfile extends Component {
   render() {
