083a0ed95e756e21a02f644620f06db1535b69b3
Jacob Hackett <jacobrhackett@gmail.com>
2017-10-11 19:40:37 -0400

    removing some routes from the api because filtering spells should all be done through the same api with query parameters

--- server/api/spells.js
+++ server/api/spells.js
@@ -26,34 +26,6 @@ class SpellEngine {
     spellDao.spells(query, res)
   }
 
-  spellsByLevel = (level, res) => {
-    if(this.validLevels.includes(level))
-      spellDao.spells({level:level}, res)
-    else
-      res.sendStatus(404)
-  }
-
-  spellsBySchool = (s, res) => {
-    const school = this._capitalize(s)
-    if(this.validSchools.includes(school))
-      spellDao.spells({school:school}, res)
-    else
-      res.sendStatus(404)
-  }
-
-  spellsByClass = (c, res) => {
-    const className = this._capitalize(c)
-    if(this.validClasses.includes(className))
-      spellDao.spells({classes:className}, res)
-    else
-      res.sendStatus(404)
-  }
-
-  // helper method to capitalize the first character in a string
-  _capitalize = word => {
-    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
-  }
-
   // helper method used to convert url query parameters to keys that match the spells schema
   _fixKey = key => {
     return key === 'class' ? 'classes' : key
--- server/routes/spells.js
+++ server/routes/spells.js
@@ -11,16 +11,4 @@ module.exports = app => {
       spellEngine.spells(res)
   })
 
-  app.get('/api/v1/spells/level/:level', (req, res) => {
-    spellEngine.spellsByLevel(req.params.level, res)
-  })
-
-  app.get('/api/v1/spells/school/:school', (req, res) => {
-    spellEngine.spellsBySchool(req.params.school, res)
-  })
-
-  app.get('/api/v1/spells/class/:className', (req, res) => {
-    spellEngine.spellsByClass(req.params.className, res)
-  })
-
 }
