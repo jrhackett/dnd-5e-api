79cdbf5ab31b87187d8db9be9aecf41bc1d93636
Jacob Hackett <jacobrhackett@gmail.com>
2017-10-21 22:53:48 -0400

    model schema formatting

--- server/db/models/spell.js
+++ server/db/models/spell.js
@@ -1,24 +1,24 @@
 const mongoose = require('mongoose')
 
 const spellSchema = mongoose.Schema({
-  school : { type: String, default: '' },
-  name : { type: String, default: '' },
-  level : { type: String, default: '' },
-  ee : { type: String, default: '' },
-  ritual : { type: String, default: '' },
-  casting_time : { type: String, default: '' },
-  scag : { type: String, default: '' },
-  id : { type: String, default: '' },
-  source : { type: String, default: '' },
-  range : { type: String, default: '' },
-  classes : [ { type: String } ],
-  components : { type: String, default: '' },
-  duration : { type: String, default: '' },
-  athigherlevel : { type: String, default: '' },
-  concentration : { type: String, default: '' },
-  slug : { type: String, default: '' },
-  page : { type: String, default: '' },
-  description : { type: String, default: '' }
+  school: { type: String, default: '' },
+  name: { type: String, default: '' },
+  level: { type: String, default: '' },
+  ee: { type: String, default: '' },
+  ritual: { type: String, default: '' },
+  casting_time: { type: String, default: '' },
+  scag: { type: String, default: '' },
+  id: { type: String, default: '' },
+  source: { type: String, default: '' },
+  range: { type: String, default: '' },
+  classes: [ { type: String } ],
+  components: { type: String, default: '' },
+  duration: { type: String, default: '' },
+  athigherlevel: { type: String, default: '' },
+  concentration: { type: String, default: '' },
+  slug: { type: String, default: '' },
+  page: { type: String, default: '' },
+  description: { type: String, default: '' }
 })
 
 spellSchema.query.byQuery = function(filter) {
