98c002c47ebbebf7228837096ecbbc6093c30d51
Jacob Hackett <jacobrhackett@gmail.com>
2017-10-28 22:31:40 -0400

    adding to file logging for http requests

--- server/src/server.js
+++ server/src/server.js
@@ -3,11 +3,14 @@ import mongoose from 'mongoose'
 import morgan from 'morgan'
 import promise from 'bluebird'
 import bodyParser from 'body-parser'
+import fs from 'fs'
+import path from 'path'
 import config from './config'
 import spellRouter from './routes/spells'
 
 const app = express()
 const port = config.port
+const accessLogStream = fs.createWriteStream(path.join(__dirname, '../logs/access.log'), {flags: 'a'})
 
 mongoose.Promise = promise
 mongoose.connect(config.database_url, { useMongoClient: true })
@@ -15,10 +18,10 @@ mongoose.connect(config.database_url, { useMongoClient: true })
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.use(morgan('dev'))
+app.use(morgan('combined', {stream: accessLogStream}))
 
 app.use('/api/v1/spells', spellRouter)
 
 const server = app.listen(port)
 
-module.exports = app // just for testing
+module.exports = app
