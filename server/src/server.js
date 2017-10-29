7da2a2280c7c5698674726e070dac154280e82da
Jacob Hackett <jacobrhackett@gmail.com>
2017-10-28 22:32:09 -0400

    minor formatting

--- server/src/server.js
+++ server/src/server.js
@@ -10,7 +10,7 @@ import spellRouter from './routes/spells'
 
 const app = express()
 const port = config.port
-const accessLogStream = fs.createWriteStream(path.join(__dirname, '../logs/access.log'), {flags: 'a'})
+const accessLogStream = fs.createWriteStream(path.join(__dirname, '../logs/access.log'), { flags: 'a' })
 
 mongoose.Promise = promise
 mongoose.connect(config.database_url, { useMongoClient: true })
@@ -18,7 +18,7 @@ mongoose.connect(config.database_url, { useMongoClient: true })
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.use(morgan('combined', {stream: accessLogStream}))
+app.use(morgan('combined', { stream: accessLogStream }))
 
 app.use('/api/v1/spells', spellRouter)
 
