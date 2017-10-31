7efbef06605540e848119ff0f9eea7b4a3138101
Jacob Hackett <jacobrhackett@gmail.com>
2017-10-31 18:06:22 -0400

    changing config.js to use let and const

--- server/src/config.js
+++ server/src/config.js
@@ -1,6 +1,6 @@
-var config = {}
+let config = {}
 
-var env = process.env.NODE_ENV || 'development'
+const env = process.env.NODE_ENV || 'development'
 
 if(env === 'development' || env === 'test' || env === 'production') {
   config = require('../env/' + env) 
