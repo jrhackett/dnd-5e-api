94b265004f8aa8bb944dc9a30f160f1c41ba9daa
Jacob Hackett <jacobrhackett@gmail.com>
2017-11-23 12:23:26 -0500

    fixing a miss this

--- server/src/api/spells.js
+++ server/src/api/spells.js
@@ -1,15 +1,31 @@
 import Spell from '../db/models/spell'
+import json2csv from 'json2csv'
 
 class SpellEngine {
 
   getSpells = (req, res) => {
-    const query = { ...Object.keys(req.query).map(key => ({ [this._fixKey(key)]: req.query[key] })) }
+    const query = this._getQuery(req.query)
     Spell.find(query).sort('name').exec(function(err, spells) {
       if(err) {
         console.log(err)
         res.sendStatus(500) // TODO form a better response than this
       }
       res.json(spells)
+    })
+  }
+
+  downloadSpells = (req, res) => {
+    const query = { ...this._getQuery(req.query), level: '8' }
+    Spell.find(query).sort('name').exec(function(err, spells) {
+      if(err) {
+        console.log(err)
+        res.sendStatus(500) // TODO form a better response than this
+      }
+      json2csv({ data: spells }, function(err, csv) {
+        res.setHeader('Content-disposition', 'attachment; filename=data.csv')
+        res.set('Content-Type', 'text/csv')
+        res.status(200).download(csv)
+      })
     })
   }
 
@@ -41,6 +57,10 @@ class SpellEngine {
       }
       res.sendStatus(204)
     })
+  }
+
+  _getQuery = query => {
+    return { ...Object.keys(query).map(key => ({ [this._fixKey(key)]: query[key] })) }
   }
 
   // helper method used to convert url query parameters to keys that match the spells schema
