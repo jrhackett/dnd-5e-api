84bfed6647e0abf3696fc9e268bdc7e8b3a29737
Jacob Hackett <jacobrhackett@gmail.com>
2018-02-04 13:22:37 -0500

    finalizing heroku deployment hopefully

--- server/env/production.js
+++ server/env/production.js
@@ -1,14 +1,4 @@
-// module.exports = {
-//   database_url: process.env.DATABASE_URL,
-//   port: process.env.PORT
-// }
-
-var envFile = __dirname + '/env.json'
-var jsonfile = require('jsonfile')
-
-var envVars = jsonfile.readFileSync(envFile)
-
 module.exports = {
-  database_url: envVars["database_url"],
-  port: envVars["port"]
+  database_url: process.env.DATABASE_URL,
+  port: process.env.PORT
 }
