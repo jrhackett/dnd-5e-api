ff38484fef1aa0be6afa3c95237648c1bafe2b1a
Jacob Hackett <jacobrhackett@gmail.com>
2018-02-04 12:56:02 -0500

    forgot to remove morgan

--- server/build/server.js
+++ server/build/server.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(morgan('combined', { stream: accessLogStream }));
+// app.use(morgan('combined', { stream: accessLogStream }))
 
 app.use('/api/v1/spells', spellRouter);
 
--- server/src/server.js
+++ server/src/server.js
@@ -20,7 +20,7 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.use(morgan('combined', { stream: accessLogStream }))
+// app.use(morgan('combined', { stream: accessLogStream }))
 
 app.use('/api/v1/spells', spellRouter)
 
