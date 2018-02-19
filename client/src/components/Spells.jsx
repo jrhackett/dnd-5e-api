9c3fdc4b53376ca87871852912d448eca5007672
Jacob Hackett <jacobrhackett@gmail.com>
2018-02-18 20:36:52 -0500

    removing spells.css from root

--- client/src/components/Spells.jsx
+++ client/src/components/Spells.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import SpellsTopBarContainer from '../containers/spells/SpellsTopBarContainer'
 import SpellsListContainer from '../containers/spells/SpellsListContainer'
-import '../styles/spells.css'
 
 const RootSpells = () => (
   <div>
