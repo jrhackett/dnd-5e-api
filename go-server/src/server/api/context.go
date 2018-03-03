28084e424afeff5b29c155902beeff22a1133bfc
Jacob Hackett <jacobrhackett@gmail.com>
2018-03-03 11:30:37 -0500

    comments

--- go-server/src/server/api/context.go
+++ go-server/src/server/api/context.go
@@ -2,6 +2,8 @@ package api
 
 import "server/api/connections"
 
+// Context describes the context in which the app is running
+// This helps us mock different connections for testing different situations
 type Context struct {
 	DB connections.DatabaseConnection
 }
--- go-server/src/server/api/routes.go
+++ go-server/src/server/api/routes.go
@@ -6,10 +6,10 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// NewRouter creates a new router
 func NewRouter(context Context) *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 
-	// TODO add auth handler to these
 	router.Methods("GET").Path("/").Name("Index").HandlerFunc(context.Index)
 	router.Methods("GET").Path("/spells").Name("Get Spells").HandlerFunc(context.GetSpells)
 	router.Methods("GET").Path("/user").Name("Get User").Handler(authHandler(http.HandlerFunc(context.GetUser)))
