5126a973a9b57b5899de19c187799dfafb6423c2
Jacob Hackett <jacobrhackett@gmail.com>
2018-03-11 14:18:10 -0400

    context comment

--- go-server/src/server/api/contexts.go
+++ go-server/src/server/api/contexts.go
@@ -2,6 +2,7 @@ package api
 
 import "server/api/connections"
 
+// SimpleContext is a baseline connection used to mock simple DB function
 var SimpleContext = Context{
 	DB: connections.MockDBConnection{},
 }
