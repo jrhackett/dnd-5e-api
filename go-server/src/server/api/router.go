f92e1c7016381111248af51a6ed3832cabca3cb3
Jacob Hackett <jacobrhackett@gmail.com>
2018-03-11 14:14:10 -0400

    rename to router

--- go-server/src/server/api/router.go
+++ go-server/src/server/api/router.go
@@ -10,7 +10,9 @@ import (
 func NewRouter(context Context) *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 
+	// TODO make index serve static files from the client bundle
 	router.Methods("GET").Path("/").Name("Index").HandlerFunc(context.Index)
+
 	router.Methods("GET").Path("/spells").Name("Get Spells").HandlerFunc(context.GetSpells)
 	router.Methods("GET").Path("/user").Name("Get User").Handler(authHandler(http.HandlerFunc(context.GetUser)))
 
