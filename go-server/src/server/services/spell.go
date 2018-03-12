e20e5eb5612b9a57c95622b50efccda45c35d408
Jacob Hackett <jacobrhackett@gmail.com>
2018-03-11 21:39:39 -0400

    typo

--- go-server/src/server/services/spell.go
+++ go-server/src/server/services/spell.go
@@ -5,7 +5,7 @@ import "server/models"
 type (
 	// spellDAO specifies the interface of the spell DAO needed by SpellService.
 	spellDAO interface {
-		// Get returns the artist with the specified spell ID.
+		// Get returns the spell with the specified spell ID.
 		Get(id int) (*models.Spell, error)
 	}
 
