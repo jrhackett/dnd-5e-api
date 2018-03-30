44623bc27ba726925804b0691c10957d2097acb3
Jacob Hackett <jacobrhackett@gmail.com>
2018-03-29 20:16:51 -0400

    adding util package

--- server/src/server/apis/spell_test.go
+++ server/src/server/apis/spell_test.go
@@ -2,15 +2,31 @@ package apis
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"server/daos"
+	"server/services"
+	"server/util"
 	"testing"
+
+	"github.com/gorilla/mux"
 )
+
+// executeRequest using the given DAO
+func executeRequest(req *http.Request, spellDaoVersion daos.SpellDAOVersion) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	router := mux.NewRouter().StrictSlash(true)
+	spellDao, _ := daos.NewSpellDAO(spellDaoVersion)
+	ServeSpellResource(router, services.NewSpellService(spellDao))
+	router.ServeHTTP(rr, req)
+
+	return rr
+}
 
 func TestSimpleMockGetSpells(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/api/v1/spells", nil)
 	response := executeRequest(req, daos.MockDB)
 
-	checkResponseCode(t, http.StatusOK, response.Code)
+	util.CheckResponseCode(t, http.StatusOK, response.Code)
 
-	assertEqual(t, response.Body.String(), "[]")
+	util.AssertEqual(t, response.Body.String(), "[]")
 }
